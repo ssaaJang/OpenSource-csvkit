4573381155a6bef4416307264d5956d512452df3
sajang <wkdtmddk6733@gmail.com>
2023-06-09 09:38:31 +0900

    csvgit&csvgpt

--- csvkit/csvkit/utilities/csvgit.py
+++ csvkit/csvkit/utilities/csvgit.py
@@ -7,7 +7,6 @@ from csvkit.cli import CSVKitUtility
 class CSVGit(CSVKitUtility):
     description = 'Upload CSV files to Github'
     
-
     def add_arguments(self):
         self.argparser.add_argument(
             'inputfile', dest='inputfile', help='Input CSV File'
@@ -23,5 +22,28 @@ class CSVGit(CSVKitUtility):
         inputfile = self.args.inputfile
         repository = self.args.repo
         token = self.args.token
+
+        gg = Github(token)
+        repo = gg.get_repo(repository)
+
+        with open(inputfile,'rb') as f:
+            ff = f.read()
+        repo.upload_file(inputfile, 'Upload CSV files to Github', ff , branch ='main')
+
+        print(f'"{inputfile}" uploaded to GitHub')
+
+    def launch_new_instance():
+        utility = CSVGit()
+        utility.run()
+
+
+    if __name__ == '__main__':
+        launch_new_instance()
+
+
+
+
+
+
         
          
---
+++ csvkit/csvkit/utilities/csvgpt.py
@@ -0,0 +1,60 @@
+import csv
+import requests
+import openai
+from csvkit.cli import CSVKitUtility
+
+openai.api_key = "insert OpenAPI Key"
+
+messages = []
+def GPT(input_text):
+        user_content = input_text
+        messages.append({"role": "user", "content": f"{user_content}"})
+
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+        assistant_content = completion.choices[0].message["content"].strip() # chatGPT 에게 받은 대답
+
+        messages.append({"role": "assistant", "content": f"{assistant_content}"})
+
+class CSVGPT(CSVKitUtility):
+    description = " If you have any questions about the data, ask the chatbot."
+    
+    def add_arguments(self):
+        self.argparser.add_argument(
+            'inputfile' , dest='inputfile',help = 'Input CSV File'
+        )
+
+    def main(self):
+        inputfile = self.args.inputfile
+
+        with open(inputfile,'r') as f:
+            r = csv.reader(f)
+            lines = [next(r) for _ in range(6)]
+        for line in lines:
+            print(line)
+        
+        #데이터를 GPT 에 먼저 넣어서 기억하도록 하기
+        GPT(lines)
+        
+
+        while True:
+            user_content = input("user : ")
+            if user_content == 'q':
+                break
+            GPT(user_content)
+        print('answer : ', messages)
+    
+    def launch_new_instance():
+        utility = CSVGPT()
+        utility.run()
+
+
+    if __name__ == '__main__':
+        launch_new_instance()
+
+
+
+            
+
+
+
